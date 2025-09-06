17d254b67910a49edd448a4633a2f69ee6f4e705
naveenyadav9515 <naveenyadav9515@gmail.com>
2025-09-06 10:08:28 +0530

    ExM-1.1.2.2 i18n initial configuration changes

---
+++ src/polyfills.ts
@@ -0,0 +1,10 @@
+/**
+ * This file includes polyfills that are required for this Angular application.
+ * Your application is running in a zoneless mode, relying on Signals for change detection.
+ */
+
+// This polyfill is required for the '$localize' function to work correctly
+// in all environments, especially during server-side rendering.
+import '@angular/localize/init';
+
+// Add any other polyfills required by your specific dependencies here.
